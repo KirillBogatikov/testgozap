ccc9063713b3c7520440759d5c9852a03d4d4135
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2021-10-18 01:09:34 +0300

    b

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"math/rand"
 	"time"
 )
+
+type S struct {
+	Name string `json:"name"`
+	Code int    `json:"code"`
+}
 
 func main() {
 	cfg := zap.NewProductionConfig()
@@ -17,7 +24,17 @@ func main() {
 	sugar := log.Sugar()
 
 	for {
-		sugar.Infow("Hello world")
+		s := &S{
+			Name: fmt.Sprintf("Ivan-%d", rand.Int()),
+			Code: rand.Intn(255),
+		}
+
+		if rand.Float64() > 0.75 {
+			sugar.Errorw("Response failed", "response", s)
+		} else {
+			sugar.Infow("Response received", "response", s)
+		}
+
 		time.Sleep(3 * time.Second)
 	}
 }
